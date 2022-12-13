6052eb21544287b573a594be341e0a8dcb8f4f12
Dmitry Sorokin <dmitrii.sorokin@phystech.edu>
2022-12-13 22:08:19 +0700

    params

--- retro_branching/src/scip_params/gasse_2019_scip_params.py
+++ retro_branching/src/scip_params/gasse_2019_scip_params.py
@@ -1,7 +1,8 @@
 gasse_2019_scip_params = {
-    'separating/maxrounds': 0,  # separate (cut) only at root node
+    'separating/maxrounds': 0,    # separate (cut) only at root node
     'presolving/maxrestarts': 0,  # disable solver restarts
-    'limits/time': 20 * 60,  # solver time limit
-    'limits/gap': 3e-4,  # 0.03% relative primal-dual gap (default: 0.0)
-    'limits/nodes': -1,
+    'limits/time': 60 * 60,       # solver time limit
+    'timing/clocktype': 1,        # 1: CPU user seconds, 2: wall clock time
+    # 'limits/gap': 3e-4,         # 0.03% relative primal-dual gap (default: 0.0)
+    # 'limits/nodes': -1,
 }
